2563c4a28de4b88a3e11bf257096779a2a0fd569
Andriy Starodub <ext-andriy.starodub@here.com>
2018-10-23 17:41:33 +0300

    Initial Commit

---
+++ scrm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scrm
+//
+//  Created by astarodub on 10/23/18.
+//  Copyright © 2018 StarAnd software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
